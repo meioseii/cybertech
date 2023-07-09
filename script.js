ecc4254a6c2d6089b9f7d43716d6f326b6f3be0a
Austin <105921882+meioseii@users.noreply.github.com>
2023-07-09 16:36:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
     switch(selectValue) {
         case 'Accounting':
-            window.location.href = './accounting/CYB_ACCOUNTING.html';
+            window.location.href = './accounting/cyb_acc_new.html';
             break;
         case 'Admin':
             window.location.href = 'admin.html';
@@ -24,13 +24,13 @@ buttons.forEach(button => {
             window.location.href = './supply_chain/index.html';
             break;
         case 'Human Resource':
-            window.location.href = '';
+            window.location.href = './human_resource/cyb_hrd_new.html';
             break;
         case 'Marketing':
             window.location.href = './marketing/cyb_mkt_new.html';
             break;
         case 'Payroll':
-            window.location.href = './payroll/payrollfront.html';
+            window.location.href = './payroll/cyb_pyr_new.html';
             break;
     }
 })
